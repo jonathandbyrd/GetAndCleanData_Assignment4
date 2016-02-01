a980a4ce7a071dd6a3168d0742c02ef5cde4ae53
jonathandbyrd <jonathan.d.byrd@live.com>
2016-02-01 05:45:34 -0500

    uploading updates to project

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # GetAndCleanData_Assignment4
 This repo holds the files to assignment 4 of the Data Science Class: Getting and Cleaning Data
+
+This document will describe what data files are needed and the operations done on those data files to produce the output desired. This document will be broken into the following sections:
+
+BACKGROUND: what the project is about
+FILES: list of necessary files
+SCRIPT(S): the name of the analysis/code scripts and what they do
+INSTRUCTIONS: what to do
+
+BACKGROUND
+        The original experiments were carried out on 30 subjects and a random group was selected to be the "training" group and the rest were by default part of the "test" group. The data was supplied as a .zip file that contain two folders: train and test which contain the file of importance. The strategy will be to require the user/tester to unzip the zip archive, rename the folder to "smartphonedata" and copy it to the working directory of R. From that point the scripts will combine the two data sets that had been separated into "test" and "train" and then selecting the variables of interest for later tidying and analysis.
+
+FILES
+        smartphonedata/test/
+                subject_test.txt
+                y_test.txt
+                X_test.txt
+        
+        smartphonedata/train/
+                subject_train.txt
+                y_train.txt
+                X_train.txt
+
+SCRIPT
+        run_analysis.R
+
+INSTRUCTIONS
+        1. unzip the data archive file: getdata-projectfiles-UCI HAR Dataset.zip
+        2. rename the resulting unzipped folder to: "smartphonedata"
+        3. copy this folder into the working directory of R
+        4. copy the script file "run_analysis.R" to the working directory of R
---
+++ run_analysis.R
@@ -0,0 +1,53 @@
+run_analysis <- function() {
+        ## load the dplyr package
+        library(dplyr)
+        
+        ## load the feature list
+        featureData <- read.delim("./UCI HAR Dataset/features.txt", sep = " ", header = FALSE, stringsAsFactors = FALSE)
+        featureRaw <- featureData[,"V2"]
+        
+        ## load the activity labels
+        activityData <- read.delim("./UCI HAR Dataset/activity_labels.txt", sep = " ", header = FALSE, stringsAsFactors = FALSE)
+        
+        ## load and combine the test data
+        testSubjects <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+        testActivity <- read.table("./UCI HAR Dataset/test/y_test.txt", col.names = "activitytested")
+        
+        ## need to add the activity label as factors
+        ## we'll do so by joining the two data frames (an inner join)
+        testActivityLabels <- merge(testActivity, activityData, by.x = "activitytested", by.y = "V1", all = FALSE)
+        
+        testdata <- read.fwf("./UCI HAR Dataset/test/X_test.txt", widths = rep(c(16), length(featureRaw)), col.names = featureRaw)
+        
+        ##select the important columns
+        subsetTestData <- select(testdata, grep("(mean|std)", featureRaw))
+        
+        combinedtest <- cbind(testSubjects, activity = testActivityLabels$V2, subsetTestData)
+        
+        ## load and combine the training data
+        trainSubjects <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+        trainActivity <- read.table("./UCI HAR Dataset/train/y_train.txt", col.names = "activitytested")
+        
+        ## need to add the activity label as factors
+        ## we'll do so by joining the two data frames (an inner join)
+        trainActivityLabels <- merge(trainActivity, activityData, by.x = "activitytested", by.y = "V1", all = FALSE)
+        
+        traindata <- read.fwf("./UCI HAR Dataset/train/X_train.txt", widths = rep(c(16), length(featureRaw)), col.names = featureRaw)
+        
+        ##select the important columns
+        subsetTrainData <- select(traindata, grep("(mean|std)", featureRaw))
+        
+        combinedtrain <- cbind(trainSubjects, activity = trainActivityLabels$V2, subsetTrainData)
+        
+        ## merge the training and test data
+        ## we'll rowbind here to combine
+        meanstddata <- rbind(combinedtest, combinedtrain)
+        
+        ## group by subject and activity
+        bySubjectByActivity <- group_by(meanstddata, subject, activitylabels)
+        
+        ## apply the mean over each column/variable of the grouped data
+        avgs <- summarize_each(bySubjectByActivity, funs(mean))
+       
+        avgs        
+}
